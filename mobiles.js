099fba7480e3998501bf47c201a9ba8ca25118da
Tarique <anwartarique408@gmail.com>
2022-08-26 10:15:02 +0530

    Added products to mob page

--- mobiles.js
+++ mobiles.js
@@ -24,3 +24,76 @@ document.getElementById('catttt_toggle').addEventListener('click',()=>{
     hide_brand.style.display = (hide_brand.style.display == 'none') ? 'block' : 'none';
     document.getElementById('catttt').classList.toggle('spin')
 })
+let z=1;
+
+let getData = async () =>{
+    try{
+        let res = await fetch('http://localhost:3000/items?category=mobile');
+        let data = await res.json();
+
+        console.log(data);
+        append(data);
+
+    }catch(err){
+        console.log(err)
+    }
+}
+getData();
+
+let container = document.querySelector('#mob_con')
+
+let append = (data)=>{
+    container.innerHTML=null;
+    for(let i =0; i<21*z; i++){
+        let div=document.createElement('div');
+        div.setAttribute('id', 'mob_divs');
+
+        let img = document.createElement('img');
+        img.src = data[i].image;
+
+        let img_div = document.createElement('div');
+        img_div.setAttribute('class', 'img_div')
+
+        let heart = document.createElement('div');
+        heart.setAttribute('class', 'heart')
+        heart.innerHTML=`<i class="fa-regular fa-heart"></i>`
+
+        img_div.append(img,heart)
+
+        let price = document.createElement('h3');
+        price.innerHTML = `₹ <span>${data[i].price}</span>`
+
+        let brand = document.createElement('p');
+        brand.setAttribute('class', 'brand_p')
+        brand.innerText = data[i].brand;
+
+        let add_div = document.createElement('div');
+        add_div.setAttribute('class', 'address_div');
+
+        let address = document.createElement('p');
+        address.innerText = data[i].address;
+
+        let date = document.createElement('p');
+        date.innerText = data[i].date;
+
+        add_div.append(address,date);
+
+        div.append(img_div,price,brand,add_div)
+        container.append(div);
+    }
+    let load_more=document.createElement('button');
+    load_more.innerText='Load More'
+    load_more.setAttribute('id','load_btn')
+    container.append(load_more)
+
+    load_more.addEventListener('click',function(){
+        z++;
+        getData();
+    })
+}
+
+// function load_more(){
+//     z=z++;
+//     console.log('mnjkk');
+//     getData();
+// }
