83f563533f3fb852719e77b876db73449186a37c
lalit_bisht <lb6995@gmail.com>
2022-08-27 15:56:49 +0530

    file

--- home_product.js
+++
@@ -1,7 +0,0 @@
-// *********************************advertise*******************
-let add_img=document.querySelector(".add_img")
-let add_icon=document.getElementById("add_icon")
-
-add_icon.addEventListener("click",()=>{
-    add_img.style="display:none"
-})
